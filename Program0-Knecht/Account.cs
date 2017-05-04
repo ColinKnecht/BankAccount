79ed1f1a8afba5e9d73b258a12e120713f47a353
Colin Knecht <cknecht87@gmail.com>
2017-05-04 13:01:41 -0400

    Initial Commit
    Console Application using Arrays to track bank account balance

---
+++ Program0-Knecht/Account.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+//COLIN KNECHT
+//PROGRAM 0 -- CPT 244
+namespace Program0_Knecht
+{
+    class Account
+    {
+        enum TransactionType {DEPOSIT,WITHDRAWAL, ADD_INTEREST, STARTING_BALANCE};
+        double startingBalance;
+        double[] transactionHistory = new double[50];
+        TransactionType[] transactionEnums = new TransactionType[50];
+        double[] transactionAmount = new double[50];
+        int index = 0;
+
+        public Account(double startingBalance)
+        {
+            this.startingBalance = startingBalance;
+            transactionHistory[index] = this.startingBalance;
+            transactionEnums[index] = TransactionType.STARTING_BALANCE;
+            transactionAmount[index] = startingBalance;
+            index++;
+        }
+
+        public void deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                amount = 0;
+            }
+            startingBalance += amount;
+            transactionHistory[index] = startingBalance;
+            transactionEnums[index] = TransactionType.DEPOSIT;
+            transactionAmount[index] = amount;
+            index++;
+        }
+
+        public void withdraw(double amount)
+        {
+           
+            if (transactionHistory[index - 1] - amount <= 0)
+            {
+                Console.WriteLine("Insufficient Funds!  You Can't Go Below 0 Dollars!!!");
+            }
+            else
+            {
+                transactionHistory[index] = transactionHistory[index - 1] - amount;
+                transactionEnums[index] = TransactionType.WITHDRAWAL;
+                transactionAmount[index] = amount;
+                index++;
+            }
+          
+        }
+
+        public void addInterest()
+        {
+            transactionHistory[index] = (transactionHistory[index - 1] * .0005) + transactionHistory[index - 1];
+            transactionEnums[index] = TransactionType.ADD_INTEREST;
+            transactionAmount[index] = transactionHistory[index - 1] * .0005;
+            index++;
+
+        }
+
+        public double getCurrentBalance()
+        {
+            return transactionHistory[index - 1];
+        }
+
+        public string getTransactionType()//return enum type in array value
+        {
+           return "" + transactionEnums[index - 1] + "";
+        }
+
+        public void showTransactions()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                Console.Write("Balance: " + transactionHistory[i] + " --------- ");
+                Console.Write("Type: " + transactionEnums[i] + "----------- ");
+                Console.Write("Amount: " + transactionAmount[i]);
+                Console.WriteLine("");
+                //Console.WriteLine("Balance: {0} " , transactionHistory[i] + " Type {1} " , transactionEnums[i] + "Amount {2} " , transactionAmount[i]);
+
+                //Console.WriteLine("");
+            }
+        }
+
+    }//END CLASS
+}
---
+++ Program0-Knecht/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+//COLIN KNECHT
+//PROGRAM 0 -- CPT 244
+namespace Program0_Knecht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char key;
+
+            Console.WriteLine("Welcome to the Bank....  What is your starting balance?");
+            String s = Console.ReadLine();
+            double startBal = Double.Parse(s);
+            Account myAccount = new Account(startBal);
+
+             Console.WriteLine("Thank You, Please select from the menu");
+             Console.WriteLine("(D)eposit---(W)ithdraw---(C)alculate Interest---(S)how Balance---(Q)uit");
+             key = Console.ReadLine()[0];
+             while (key != 'q' && key != 'Q')
+             {
+                  if (key == 'd' || key == 'D')
+                  {
+                     Console.WriteLine("How much would you like to deposit?");
+                     String dep = Console.ReadLine();
+                     double deposit = Double.Parse(dep);
+                    if (deposit <= 0)
+                    {
+                        Console.WriteLine("You Can't deposit negative numbers!");
+                        Console.WriteLine("(D)eposit---(W)ithdraw---(C)alculate Interest---(S)how Balance---(Q)uit");
+                        key = Console.ReadLine()[0];
+                    }
+                     myAccount.deposit(deposit);
+
+                     Console.WriteLine("(D)eposit---(W)ithdraw---(C)alculate Interest---(S)how Balance---(Q)uit");
+                     key = Console.ReadLine()[0];
+                   }
+
+                        else if (key == 'w'|| key == 'W')
+                        {
+                            Console.WriteLine("WITHDRAW");
+                            Console.WriteLine("How much would you like to withdraw?");
+                            string wit = Console.ReadLine();
+                            double withdraw = Double.Parse(wit);
+                            if (withdraw <= 0)
+                            {
+                        Console.WriteLine("You Can't Withdraw 0 Dollars");
+                            }
+                            myAccount.withdraw(withdraw);
+
+                            Console.WriteLine("(D)eposit---(W)ithdraw---(C)alculate Interest---(S)how Balance---(Q)uit");
+                            key = Console.ReadLine()[0];
+                        }
+
+                        else if (key == 'c' || key == 'C')
+                        {
+                            Console.WriteLine("CALCULATE INTEREST");
+                            Console.WriteLine("Adding Interest @ .05%");
+                            myAccount.addInterest();
+
+                            Console.WriteLine("(D)eposit---(W)ithdraw---(C)alculate Interest---(S)how Balance---(Q)uit");
+                            key = Console.ReadLine()[0];
+                        }
+
+                        else if (key == 's' || key == 'S')
+                        {
+                            Console.WriteLine("SHOW BALANCE");
+                            Console.WriteLine("Your Current Balance is " + myAccount.getCurrentBalance());
+
+                            Console.WriteLine("(D)eposit---(W)ithdraw---(C)alculate Interest---(S)how Balance---(Q)uit");
+                            key = Console.ReadLine()[0];
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Invalid Key, Please select from the menu!!");
+
+                            Console.WriteLine("(D)eposit---(W)ithdraw---(C)alculate Interest---(S)how Balance---(Q)uit");
+                            key = Console.ReadLine()[0];
+                        }
+                    }//end while
+                
+  
+            Console.WriteLine("Thank You for Banking With Us. Here Is Your List of Transactions.");
+            myAccount.showTransactions();
+            Console.ReadKey();
+        }
+    }
+}
